0ec18a58e27db12d71c3077b5350d614f2da59f9
Jeff Turcotte <jeff.turcotte@gmail.com>
2016-06-12 19:44:13 -0400

    Add scripts dir and composer autoloader

--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
+if (file_exists(dirname(__DIR__) . '/vendor/autoload.php')) {
+    require_once(dirname(__DIR__) . '/vendor/autoload.php');
+}
+
 define('WP_USE_THEMES', true);
 require(__DIR__ . '/wordpress/wp-blog-header.php');
